b44227d6eccf56949d3d25481312735c5f672643
CaoMinhThuan <caominhthuan2506@gmail.com>
2023-03-09 13:44:55 +0700

    bai71

---
+++ bai71.js
@@ -0,0 +1,56 @@
+// let i = 10
+// let f = 20.5
+// let b = true
+// let s = 'Hà Nội'
+//
+// document.write('i= ' + i)
+// document.write('<br/>')
+// document.write('f= ' + f)
+// document.write('<br/>')
+// document.write('b= ' + b)
+// document.write('<br/>')
+// document.write('s= ' + s)
+
+// let width = 20
+// let heigh = 30
+// let area = width * heigh
+// document.write('Area= ' +  area )
+
+// a=prompt('nhap gia tri cua a');
+// b=prompt('nhap gia tri cua b');
+// if (a%b==0){alert('a la boi so cua b')}
+//     else {alert('a khong la boi so cua b')}
+// ;
+// let Width = prompt('nhap gia tri chieu rong') // prompt : hien bang nhap
+// let Height = prompt('nhap gia tri chieu dai')
+//
+// let inputwidth = parseInt(Width); //parseInt : gia tri so
+// let inputheight = parseInt(Height)
+// let area = inputwidth * inputheight
+// document.write("dien tich la:" +area)
+//
+
+// let vatly =+prompt('nhap diem mon vat ly');
+// let hoahoc = +prompt('nhap diem mon hoa hoc');
+// let sinhhoc = +prompt('nhap diem mon sinh hoc');
+// let trungbinh = (vatly + hoahoc + sinhhoc)/3
+// document.write('diem trung binh la:' + trungbinh);
+
+// let doC = +prompt('nhap gia tri do C');
+// let doF = 9*doC/5+32;
+// document.write('Gia tri nhiet do f la:'+doF
+// );
+
+//dien tich hinh tron S = πR2
+
+// let bankinh = +prompt('ban kinh hinh tron');
+// const pi = 3.14
+// let dientich = 3.14 * bankinh * bankinh
+// document.write('dien tich hinh tron =' + dientich)
+
+//chu vi hinh tron  C = 2Pi x R
+
+let bankinh = +prompt('ban kinh hinh tron');
+const pi = 3.14;
+let chuvi = 2*3.14*bankinh;
+document.write('chu vi hinh tron ='+chuvi);
